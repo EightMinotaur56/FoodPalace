45eefc113e648a50dbf824eb8343d17bb09d24e8
EightMinotaur56 <sos.tomi9@gmail.com>
2023-11-14 07:18:22 +0100

    changed dbname

--- src/config.php
+++ src/config.php
@@ -1,20 +1,15 @@
 <?php
 
-define('BASE_URL', 'http://restaurant.localhost/');
+define('BASE_URL', 'http://foodpalace.localhost/');
 
 define('BASE_PATH', __DIR__);
 
-
-$ipAddress="130.211.57.2";
-   $user="root";
-   $password="";
-   $db="restaurant";
 
 define('DB_HOST','130.211.57.2');
 define('DB_PORT',3306);
 define('DB_USER','root');
 define('DB_PASS','');
-define('DB_NAME','restaurant');
+define('DB_NAME','foodpalace');
 
 
 
