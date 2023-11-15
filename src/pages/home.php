04a15ff53477de29fb4bd886885140efb99a26b7
EightMinotaur56 <sos.tomi9@gmail.com>
2023-11-15 18:08:33 +0100

    updated home and other css

--- src/pages/home.php
+++ src/pages/home.php
@@ -5,22 +5,25 @@
     $select->execute();
     $results=$select->fetchAll(PDO::FETCH_ASSOC);
 ?>
-
-<p>Home Page!</p>
-<?php foreach($results as $result):?>
-    <div>
-        ID: <?php echo $result['id'];?>
+<div class="home">
+    <div class="content">
+        <p>Home Page!</p>
+        <?php foreach($results as $result):?>
+            <div>
+                ID: <?php echo $result['id'];?>
+            </div>
+            <div>
+                Client Name: <?php echo $result['clientName'];?>
+            </div>
+            <div>
+                Client Phone: <?php echo $result['clientPhone'];?>
+            </div>
+            <div>
+                Party Size: <?php echo $result['partySize'];?>
+            </div>
+            <div>
+                Reservation Date: <?php echo $result['reservationDate'];?>
+            </div>
+        <?php endforeach;?>
     </div>
-    <div>
-        Client Name: <?php echo $result['clientName'];?>
-    </div>
-    <div>
-        Client Phone: <?php echo $result['clientPhone'];?>
-    </div>
-    <div>
-        Party Size: <?php echo $result['partySize'];?>
-    </div>
-    <div>
-        Reservation Date: <?php echo $result['reservationDate'];?>
-    </div>
-<?php endforeach;?>
+</div>
--- src/views/_header.php
+++ src/views/_header.php
@@ -4,7 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= asset_url('styles/style.css'); ?>">
+    <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" 
+        integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" 
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="<?= asset_url('css/app.css'); ?>">
     <script src="https://kit.fontawesome.com/5f4f01cc86.js" crossorigin="anonymous"></script>
 </head>
 <Title>Food Palace</Title>
@@ -12,4 +17,21 @@
 
 
 <body>
-    <main class="container">
+    <div class="header">
+        <div class="content">
+            <div class="logo">
+                <ul>
+                    <li><image src="./images/logo.png" alt=""></li>
+                    <li><h1>Palace</h1></li>
+                </ul>
+            </div>
+            <div class="navigation">
+                <ul>
+                    <li><a href="<?php echo page_url('home'); ?>">Home</a></li>
+                    <li><a href="<?= page_url('about'); ?>">About</a></li>
+                    <li><a href="<?= page_url('contact'); ?>">Contact</a></li>
+                </ul>
+                <button class="reservation"><a href="<?= page_url('reservation'); ?>" role="button">Reservation</a></button>
+            </div>
+        </div>
+    </div>
