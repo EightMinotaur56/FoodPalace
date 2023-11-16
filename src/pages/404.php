7714565a37ff7a73fda2c577a905e530cf7af8a9
EightMinotaur56 <sos.tomi9@gmail.com>
2023-11-16 12:55:53 +0100

    added contacts page and 404 page now displays correctly

--- src/pages/404.php
+++ src/pages/404.php
@@ -1,10 +1,18 @@
+<?php
+?>
 <title>404</title>
-<h1>
-    404, page not found
-</h1>
 
-<p>
-    <a href="http://localhost/?page=home" role="button">
-        Go back to Home
-    </a>
-</p>
+<div class="pageNotFound">
+    <div class="content">
+        <h1>
+            404, page not found
+        </h1>
+        <p>
+            <button type="button" class="backHome">
+                <a href="<?= page_url('home') ?>" role="button">
+                    Go back to Home
+                </a>
+            </button>
+        </p>
+    </div>
+</div>
--- src/views/_header.php
+++ src/views/_header.php
@@ -10,8 +10,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" 
         integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" 
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <link rel="stylesheet" href="<?= asset_url('css/app.css'); ?>">
+    <link rel="stylesheet" href="css/app.css">
     <script src="https://kit.fontawesome.com/5f4f01cc86.js" crossorigin="anonymous"></script>
 </head>
 <Title>Food Palace</Title>
