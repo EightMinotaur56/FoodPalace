d9862a948f3f1971a4c7096d18bea9aaaad0a4cd
EightMinotaur56 <sos.tomi9@gmail.com>
2023-11-16 13:39:19 +0100

    added fontawesome icons

--- src/pages/contact.php
+++ src/pages/contact.php
@@ -8,6 +8,9 @@
             <h1>CONTACT US</h1>
         </div>
         <div class="box">
+        <i class="fa-solid fa-phone-volume fa-2xl" style="color: #f9c416;"></i><h2></h2>
+        <i class="fa-solid fa-envelope fa-2xl" style="color: #f9c416;"></i>
+        <i class="fa-solid fa-location-dot fa-2xl" style="color: #f9c416;"></i>
 
         </div>
     </div>
