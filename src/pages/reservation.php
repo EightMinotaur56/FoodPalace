047ee20ed2ca9dbd49d1046ac5554ba428949d9d
EightMinotaur56 <sos.tomi9@gmail.com>
2023-11-22 19:37:42 +0100

    Changed reservation time to a drop down from 12:00 to 22:00 with 30 minute intervals

--- src/pages/reservation.php
+++ src/pages/reservation.php
@@ -57,7 +57,31 @@
                         <input type="date" name="reservationDate" value="<?= $reservationDate?>">
                     </div>
                     <div>
-                        <input type="time" name="time" value="<?= $time?>">
+                        <select name="time" style="min-width:200px">
+                            <option value=""></option>
+                            <option value="12:00">12:00</option>
+                            <option value="12:30">12:30</option>
+                            <option value="13:00">13:00</option>
+                            <option value="13:30">13:30</option>
+                            <option value="14:00">14:00</option>
+                            <option value="14:30">14:30</option>
+                            <option value="15:00">15:00</option>
+                            <option value="15:30">15:30</option>
+                            <option value="16:00">16:00</option>
+                            <option value="16:30">16:30</option>
+                            <option value="17:00">17:00</option>
+                            <option value="17:30">17:30</option>
+                            <option value="18:00">18:00</option>
+                            <option value="18:30">18:30</option>
+                            <option value="19:00">19:00</option>
+                            <option value="19:30">19:30</option>
+                            <option value="20:00">20:00</option>
+                            <option value="20:30">20:30</option>
+                            <option value="21:00">21:00</option>
+                            <option value="21:30">21:30</option>
+                            <option value="22:00">22:00</option>
+                        </select>
+                        <!--<input type="time" min="12:00" max="21:00" step="1800" name="time" value="<?= $time?>">-->
                     </div>
                     <button type="submit">Submit</button>
                 </form>
