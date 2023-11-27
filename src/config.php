9547354e6144bc6de3b89e14e9ad6febb7d3e980
EightMinotaur56 <sos.tomi9@gmail.com>
2023-11-27 19:05:26 +0100

    updated dbpassword and added database files in case you guys want them locally

--- src/config.php
+++ src/config.php
@@ -5,11 +5,11 @@ define('BASE_URL', 'http://foodpalace.localhost/');
 define('BASE_PATH', __DIR__);
 
 
-define('DB_HOST','130.211.57.2');
-define('DB_PORT',3306);
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_HOST','127.0.0.1');
+define('DB_PORT',5500);
 define('DB_NAME','foodpalace');
+define('DB_USER','root');
+define('DB_PASS','foodpalace');
 
 
 
