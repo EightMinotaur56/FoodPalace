638a2531784d59ff034d07ac97011d5297e0d6eb
EightMinotaur56 <sos.tomi9@gmail.com>
2023-11-30 13:44:23 +0100

    updated the bottom navigations css

--- src/views/_footer.php
+++ src/views/_footer.php
@@ -5,13 +5,27 @@
     $page=$page[1];
     $category=$_POST['category']??null;
     $previous=explode('=',$previousPage)[1];
+
+    $isFirst=false;
+    if($previous==='menu'){
+        $isFirst=true;
+    }
+    $cssFirst='footerNavButtonFirst';
+    if(!$isFirst){
+        $cssFirst='footerNavButton';
+    }
+    $cssLast='footerNavButton';
+    if($page==='fullMenu'){
+        $cssLast='footerNavButtonLast';
+    }
 ?>
 <?php if($page==='vegan'|| $page==='desserts'||$page==='main'||$page==='fullMenu'):?>
+    <div>
     <div style="display: flex;flex-driection:column;">
     <?php if($previous!=='menu'):?>
-    <a href="<?=page_url('menu')?>"><button type="button" class='footerNavButton'>Menu Category Seletion</button></a>
+    <a href="<?=page_url('menu')?>"><button type="button" class='footerNavButtonFirst' style="width:max-content">Menu Category Seletion</button></a>
     <?php endif;?>
-    <a href="<?=page_url($previous)?>"><button type="button" class='footerNavButton'>Go Back</button></a>
+    <a href="<?=page_url($previous)?>"><button type="button" class='<?=$cssFirst?>'>Go Back</button></a>
     <?php if($page!=='vegan'):?>
         <a href="<?=page_url('vegan')?>"><button type="button" class='footerNavButton'>Vegan</button></a>
     <?php endif;?>
@@ -19,11 +33,12 @@
         <a href="<?=page_url('main')?>"><button type="button" class='footerNavButton'>Main</button></a>
     <?php endif;?>
     <?php if($page!=='desserts'):?>
-        <a href="<?=page_url('desserts')?>"><button type="button" class='footerNavButton'>Desserts</button></a>
+        <a href="<?=page_url('desserts')?>"><button type="button" class='<?=$cssLast?>'>Desserts</button></a>
     <?php endif;?>
     <?php if($page!=='fullMenu'):?>
-        <a href="<?=page_url('fullMenu')?>"><button type="button" class='footerNavButton' style="width:max-content">Complete Menu</button></a>
+        <a href="<?=page_url('fullMenu')?>"><button type="button" class='footerNavButtonLast' style="width:max-content">Complete Menu</button></a>
     <?php endif;?>
+    </div>
     </div>
 <?php endif;?>
 </div>
