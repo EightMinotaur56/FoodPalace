9aed8103c64c7727ad14a2f494f7f7a99a31294a
EightMinotaur56 <sos.tomi9@gmail.com>
2023-11-30 09:32:42 +0100

    ...

--- src/views/_footer.php
+++ src/views/_footer.php
@@ -3,9 +3,12 @@
     $_SERVER['REQUEST_URI'];
     $page=explode("=",$link);
     $page=$page[1];
+    $category=$_POST['category']??null;
 ?>
 <?php if($page==='vegan'|| $page==='desserts'||$page==='main'||$page==='fullMenu'):?>
+    <div style="display: flex;flex-driection:column;">
     <a href="<?=page_url('menu')?>"><button type="button" class='goToFullMenu'>Back To Menu Page</button></a>
+    </div>
 <?php endif;?>
 </div>
 </body>
