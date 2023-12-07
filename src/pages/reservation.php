04d6c78025bfe7dd1b2d0b9f46c20f8c3df35dd2
luyanda23 <candic627@gmail.com>
2023-12-07 16:53:44 +0100

    name

--- src/pages/reservation.php
+++ src/pages/reservation.php
@@ -42,14 +42,14 @@
                     <div class="name">
                         <input type="text" name="clientPhone" value="<?= $clientPhone?>" placeholder="Phone Number">
                     </div>
-                    <div class="name">
+                    <div >
                         <label for="partySize">How many guests?</label>
                         <input type="number" name="partySize" value="<?= $partySize?>" placeholder="1">
                     </div>
-                    <div class="name">
+                    <div >
                         <input type="date" name="reservationDate" value="<?= $reservationDate?>">
                     </div>
-                    <div class="name">
+                    <div >
                         <select name="time" style="min-width:200px">
                             <option value=""></option>
                             <option value="12:00">12:00</option>
