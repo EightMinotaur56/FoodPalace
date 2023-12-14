3e1f1a495858b81e8b477b96515490a3d638212f
Omar Takassa <146325782+OmarTakassa@users.noreply.github.com>
2023-12-14 15:37:49 +0100

    contact.php
    Signed-off-by: Omar Takassa <146325782+OmarTakassa@users.noreply.github.com>

--- src/pages/contact.php
+++ src/pages/contact.php
@@ -1,1 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
 
+
+<body>
+
+
+    <div class="contact-container">
+        <div class="contact-left">
+            <div class="contact-info">
+                <h2>Visit Us or <span>Call Us Today</span> </h2>
+                <p class="opening-hours">Opening Hours:</p>
+                <div class="days">
+                    <h4>Tuesday - Thursday: 9am - 5pm</h4>
+                    <h4>Friday: 9am - 7pm</h4>
+                    <h4>Saturday: 8am - 4pm</h4>
+                    <h4>Sunday - Monday: Closed</h4>
+                </div>
+            </div>
+            <form class="contact-form" method="post">
+                <input type="text" name="email" placeholder="Email">
+                <input type="text" name="phone" placeholder="Phone Number">
+                <textarea name="message" placeholder="Message"></textarea>
+                <button type="submit" name="submit">Submit</button>
+            </form>
+            <div class="contact-icons">
+                <div class="icon">
+                    <i class="fas fa-phone-alt"></i>
+                    <p>Phone</p>
+                </div>
+                <div class="icon">
+                    <i class="far fa-envelope"></i>
+                    <p>Email</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <p>Location</p>
+                </div>
+            </div>
+        </div>
+        <div class="contact-right">
+            
+        </div>
+    </div>
+
+</body>
+
+</html>
